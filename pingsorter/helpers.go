97c547f21748ec6d3ab27e2deec31db9b953a2c8
QuakePhil <phil.nyc@gmail.com>
2022-11-19 16:08:13 -0500

    cleanup

--- pingsorter/helpers.go
+++ pingsorter/helpers.go
@@ -2,16 +2,27 @@ package main
 
 import (
 	"encoding/csv"
+	"github.com/go-ping/ping"
 	"log"
 	"os"
 	"sort"
 	"strings"
+	"time"
 )
 
-func check(e error) {
-	if e != nil {
-		panic(e)
+func pingHost(host string) time.Duration {
+	pinger, err := ping.NewPinger(host)
+	if err != nil {
+		log.Println(err)
 	}
+	pinger.Count = 1
+	pinger.Timeout = config.timeout
+	err = pinger.Run() // Blocks until finished.
+	if err != nil {
+		log.Println(err)
+	}
+	stats := pinger.Statistics() // get send/receive/duplicate/rtt stats
+	return stats.AvgRtt
 }
 
 func sorter(pings []registrar) {
@@ -61,5 +72,11 @@ func registrarFromRecord(record []string) registrar {
 		record[3],
 		record[4],
 		0,
+	}
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
 	}
 }
