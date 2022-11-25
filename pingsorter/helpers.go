b945abd276d80e603e87f50d20bea4514f53dc59
QuakePhil <phil.nyc@gmail.com>
2022-11-25 08:27:37 -0500

    organize

--- pingsorter/helpers.go
+++ pingsorter/helpers.go
@@ -10,6 +10,47 @@ import (
 	"time"
 )
 
+func getUniqueRecords(in chan<- registrar) {
+	records := getRecords()
+	checkDupes := make(map[string]bool)
+
+	for _, record := range records {
+		if config.linesToSkip > 0 {
+			config.linesToSkip--
+			continue
+		}
+		domainName := strings.ToLower(record[4])
+		if _, exists := checkDupes[domainName]; !exists {
+			checkDupes[domainName] = true
+			in <- registrarFromRecord(record)
+		}
+	}
+}
+
+func getRecords() (records [][]string) {
+	log.Println("reading", config.source)
+	f, err := os.Open(config.source)
+	check(err)
+	defer f.Close()
+
+	input := csv.NewReader(f)
+	input.LazyQuotes = true
+	records, err = input.ReadAll()
+	check(err)
+	return
+}
+
+func registrarFromRecord(record []string) registrar {
+	return registrar{
+		record[0],
+		record[1],
+		record[2],
+		record[3],
+		record[4],
+		0,
+	}
+}
+
 func pingHost(host string) time.Duration {
 	pinger, err := ping.NewPinger(host)
 	if err != nil {
@@ -34,46 +75,6 @@ func sorter(pings []registrar) {
 	})
 }
 
-func getRecords() (records [][]string) {
-	log.Println("reading", config.source)
-	f, err := os.Open(config.source)
-	check(err)
-	defer f.Close()
-
-	input := csv.NewReader(f)
-	input.LazyQuotes = true
-	records, err = input.ReadAll()
-	check(err)
-	return
-}
-
-func getUniqueRecords(in chan<- registrar) {
-	records := getRecords()
-	checkDupes := make(map[string]bool)
-
-	for _, record := range records {
-		if config.linesToSkip > 0 {
-			config.linesToSkip--
-			continue
-		}
-		domainName := strings.ToLower(record[4])
-		if _, exists := checkDupes[domainName]; !exists {
-			checkDupes[domainName] = true
-			in <- registrarFromRecord(record)
-		}
-	}
-}
-
-func registrarFromRecord(record []string) registrar {
-	return registrar{
-		record[0],
-		record[1],
-		record[2],
-		record[3],
-		record[4],
-		0,
-	}
-}
 
 func check(e error) {
 	if e != nil {
